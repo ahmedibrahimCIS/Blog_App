49dbfd378742f1fc22c8c88c98113f674b2c0111
Ahmed Ibrahim <ae5444404@gmail.com>
2024-12-10 09:34:31 +0200

    Configured environment variables

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import errorHandler from "./src/middleware/errorHandler.js"
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
 app.use(express.json());
--- src/DB/dbConnection.js
+++ src/DB/dbConnection.js
@@ -2,11 +2,11 @@ import mysql from "mysql2";
 
 //connection
 const connectionDB = mysql.createConnection({
-    host:"localhost",
-    port:3307,
-    user:"root",
-    password:"root",
-    database:"blog_app"
+    host:process.env.DB_HOST,
+    port:process.env.DB_PORT,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME
 })
 
 connectionDB.connect((error)=>{
